90cddfe902448f56ae9b599856116068cb9cc371
chris <selfawarecpu21@gmail.com>
2022-10-07 12:08:39 -0500

    commit 3rd week of spa studio

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -1,16 +1,34 @@
 package org.launchcode.spaday.controllers;
 
 
+import org.launchcode.spaday.models.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
 
-    @RequestMapping("add")
+    @GetMapping("add")
     public String displayAddUserForm(){
+        return "user/add";
+    }
 
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
+        model.addAttribute("user",user );
+        model.addAttribute("email", user.getEmail());
+        model.addAttribute("username",user.getUsername());
+        model.addAttribute("verify",verify);
+
+        if(verify.equals(user.getPassword())){
+            return "user/index.html";
+        }
+
+
+        return "";
     }
 
 
