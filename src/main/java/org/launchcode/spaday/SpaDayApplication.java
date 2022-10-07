1f9a9fd59d6d76c9281fa125fc33503f9fed9554
chris <selfawarecpu21@gmail.com>
2022-10-06 19:18:48 -0500

    first commit

--- src/main/java/org/launchcode/spaday/SpaDayApplication.java
+++ src/main/java/org/launchcode/spaday/SpaDayApplication.java
@@ -2,12 +2,15 @@ package org.launchcode.spaday;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 public class SpaDayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpaDayApplication.class, args);
+
 	}
 
 }
